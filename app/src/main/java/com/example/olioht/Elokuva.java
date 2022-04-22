894a5e7519829bc09af27c39c8e85d57a00f5645
jerearokivi <31415926535>
2022-04-22 18:36:56 +0300

    kommittaus

---
+++ app/src/main/java/com/example/olioht/Elokuva.java
@@ -0,0 +1,15 @@
+package com.example.olioht;
+
+import java.util.ArrayList;
+
+public class Elokuva {
+    private String nimi;
+    private String aika;
+    private int julkaisuvuosi;
+    private int kesto;
+    private int ikaRaja;
+    private ArrayList<String> genret = new ArrayList<String>();
+    private int ID;
+    private String kuvateksti;
+
+}
