77e49fb7b38bc644483556847892b7346bdeb06f
MaxWesamaa <Max.Wesamaa@student.lut.fi>
2022-04-23 17:26:15 +0300

    Elokuva.java

--- app/src/main/java/com/example/olioht/Elokuva.java
+++ app/src/main/java/com/example/olioht/Elokuva.java
@@ -3,24 +3,65 @@ package com.example.olioht;
 import java.util.ArrayList;
 
 public class Elokuva {
-    private String nimi;
-    private String aika;
-    private int julkaisuvuosi;
-    private int kesto;
-    private int ikaRaja;
-    private ArrayList<String> genret = new ArrayList<String>();
+    private String name;
+    private String time;
+    private int releaseYear;
+    private int duration;
+    private int ageLimit;
+    private ArrayList<String> genres = new ArrayList<String>();
     private int ID;
-    private String kuvateksti;
+    private String picText;
 
-    public Elokuva(String nimi, String aika, int julkaisuvuosi, int kesto, int ikaRaja, ArrayList<String> genret, int ID, String kuvateksti) {
-        this.nimi = nimi;
-        this.aika = aika;
-        this.julkaisuvuosi = julkaisuvuosi;
-        this.kesto = kesto;
-        this.ikaRaja = ikaRaja;
-        this.genret = genret;
+    public Elokuva(String name, String time, int releaseYear, int duration, int ageLimit, ArrayList<String> genres, int ID, String picText) {
+        this.name = name;
+        this.time = time;
+        this.releaseYear = releaseYear;
+        this.duration = duration;
+        this.ageLimit = ageLimit;
+        this.genres = genres;
         this.ID = ID;
-        this.kuvateksti = kuvateksti;
+        this.picText = picText;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time) { this.time = time; }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+    public void setReleaseYear(int releaseYear) { this.releaseYear = releaseYear; }
+
+    public int getDuration() { return duration; }
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getAgeLimit() { return ageLimit; }
+    public void setAgeLimit(int ageLimit) {
+        this.ageLimit = ageLimit;
+    }
+
+    public int getID() { return ID; }
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public ArrayList getGenres() { return genres; }
+    public void setGenres(String genres) { this.genres = genres; }
+
+    public String getPicText() {
+        return picText;
+    }
+    public void setPicText(String picText) {
+        this.picText = picText;
     }
 
 }
