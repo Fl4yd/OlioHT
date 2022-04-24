da68da59fb926d369dd8bf90c8b010d889466d48
bombo123 <martidimamma@gmail.com>
2022-04-24 20:38:37 +0300

    historyä tein

---
+++ app/src/main/java/com/example/olioht/HistoryActivity.java
@@ -0,0 +1,19 @@
+package com.example.olioht;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HistoryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_history);
+
+
+    }
+}
