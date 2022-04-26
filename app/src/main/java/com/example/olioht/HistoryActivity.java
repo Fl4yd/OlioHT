2f55e6ddf94b535c9d682a593489c021549660ca
bombo123 <martidimamma@gmail.com>
2022-04-26 16:34:38 +0300

    historyn list tiedostot

--- app/src/main/java/com/example/olioht/HistoryActivity.java
+++ app/src/main/java/com/example/olioht/HistoryActivity.java
@@ -1,17 +1,28 @@
 package com.example.olioht;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HistoryActivity extends AppCompatActivity {
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_history);
+        setContentView(R.layout.activity_listview);
+
+
 
 
     }
---
+++ app/src/main/java/com/example/olioht/ListAdapter.java
@@ -0,0 +1,61 @@
+package com.example.olioht;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class ListAdapter extends BaseAdapter {
+
+    Context context;
+    int id;
+    String[] name;
+    String[] image;
+
+    LayoutInflater inflater;
+
+    public ListAdapter(Context context, String[] name, String[] image) {
+        this.context = context;
+        this.name = name;
+        this.image = image;
+    }
+
+    @Override
+    public int getCount() {
+        return name.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (inflater == null) {
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        if (view == null) {
+
+            view = inflater.inflate(R.layout.list_item, null);
+        }
+
+        ImageView imageView = view.findViewById(R.id.list_image);
+        TextView textView = view.findViewById(R.id.movie_name);
+
+        Picasso.get().load(image[i]).into(imageView);
+        textView.setText(name[i]);
+
+        return view;
+    }
+}
