59422b87d938f1c13ae1d138644979144fa3b40b
bombo123 <martidimamma@gmail.com>
2022-04-28 01:27:15 +0300

    User.java lisätty tiedostoihin.

---
+++ app/src/main/java/com/example/olioht/User.java
@@ -0,0 +1,41 @@
+package com.example.olioht;
+
+public class User {
+    private String mUser;
+    private String mTheatre;
+    private String mName;
+    private String mAge;
+
+    public String getmUser() {
+        return mUser;
+    }
+
+    public void setmUser(String mUser) {
+        this.mUser = mUser;
+    }
+
+    public String getmTheatre() {
+        return mTheatre;
+    }
+
+    public void setmTheatre(String mTheatre) {
+        this.mTheatre = mTheatre;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmAge() {
+        return mAge;
+    }
+
+    public void setmAge(String mAge) {
+        this.mAge = mAge;
+    }
+
+}
--- app/src/main/java/com/example/olioht/UserinfoBase.java
+++ app/src/main/java/com/example/olioht/UserinfoBase.java
@@ -39,42 +39,4 @@ public class UserinfoBase {
         return(mUsersBase);
     }
 }
-class User {
-    private String mUser;
-    private String mTheatre;
-    private String mName;
-    private String mAge;
 
-    public String getmUser() {
-        return mUser;
-    }
-
-    public void setmUser(String mUser) {
-        this.mUser = mUser;
-    }
-
-    public String getmTheatre() {
-        return mTheatre;
-    }
-
-    public void setmTheatre(String mTheatre) {
-        this.mTheatre = mTheatre;
-    }
-
-    public String getmName() {
-        return mName;
-    }
-
-    public void setmName(String mName) {
-        this.mName = mName;
-    }
-
-    public String getmAge() {
-        return mAge;
-    }
-
-    public void setmAge(String mAge) {
-        this.mAge = mAge;
-    }
-
-}
